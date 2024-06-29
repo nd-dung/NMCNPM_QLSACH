9be4ee5cdecdc309019f320e76a605afb40392dd
iannguyen25 <tuananhworks@gmail.com>
2024-06-29 15:12:53 +0700

    Add project files.

---
+++ BTL_NMCNPM_SE.G19_QLNSNhaNam/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_NMCNPM_SE.G19_QLNSNhaNam
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BTL_NMCNPM_SE.G19_QLNSNhaNam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BTL_NMCNPM_SE.G19_QLNSNhaNam.MvcApplication" Language="C#" %>
