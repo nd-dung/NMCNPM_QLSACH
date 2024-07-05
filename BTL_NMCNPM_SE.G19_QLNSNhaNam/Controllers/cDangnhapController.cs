4d89e0c49d06eb058fbb0c3900a8f7365b3bdf62
iannguyen25 <tuananhworks@gmail.com>
2024-07-05 20:53:53 +0700

    Add login

---
+++ BTL_NMCNPM_SE.G19_QLNSNhaNam/Controllers/cDangnhapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_NMCNPM_SE.G19_QLNSNhaNam.Controllers
+{
+    public class cDangnhapController : Controller
+    {
+        // GET: cDangnhap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
