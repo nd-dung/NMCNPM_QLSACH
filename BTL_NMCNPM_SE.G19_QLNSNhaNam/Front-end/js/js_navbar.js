b7a0b9d750cf70d3c248f7f79a5e84a600ce355b
iannguyen25 <tuananhworks@gmail.com>
2024-07-05 20:14:32 +0700

    Thêm front-end để xử lý giao diện

---
+++ BTL_NMCNPM_SE.G19_QLNSNhaNam/Front-end/js/js_navbar.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const mobileDropdown = document.querySelector('.mobile-dropdown');
+
+
+    if (hamburger) {
+        hamburger.addEventListener('click', function () {
+            mobileMenu.classList.toggle('active');
+        });
+    } else {
+    }
+
+    if (mobileDropdown) {
+        mobileDropdown.addEventListener('click', function (e) {
+            e.preventDefault();
+            this.querySelector('.mobile-submenu').classList.toggle('active');
+        });
+    } else {
+    }
+});
