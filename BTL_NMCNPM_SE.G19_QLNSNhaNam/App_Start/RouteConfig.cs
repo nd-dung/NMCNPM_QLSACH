efb9c0afc2f1507a0954a11ad524329edf8e3189
iannguyen25 <tuananhworks@gmail.com>
2024-07-05 20:11:07 +0700

    Sua default Routeconfig.cs

--- BTL_NMCNPM_SE.G19_QLNSNhaNam/App_Start/RouteConfig.cs
+++ BTL_NMCNPM_SE.G19_QLNSNhaNam/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BTL_NMCNPM_SE.G19_QLNSNhaNam
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "cBanhang", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
