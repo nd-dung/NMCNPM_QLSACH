dd5968c0e5bb8143763d30572463ec6e95564275
thankute <thanhpt2612@gmail.com>
2024-07-12 00:54:56 +0700

    chuc nang phan quan ly sach

--- BTL_NMCNPM_SE.G19_QLNSNhaNam/Controllers/cSachController.cs
+++ BTL_NMCNPM_SE.G19_QLNSNhaNam/Controllers/cSachController.cs
@@ -9,9 +9,53 @@ namespace BTL_NMCNPM_SE.G19_QLNSNhaNam.Models
     public class cSachController : Controller
     {
         // GET: cSach
-        public ActionResult Index()
+        DatabaseEntities db = new DatabaseEntities();
+
+        public ActionResult Index(string searchInput="")
         {
-            return View("~/Views/cSach/vSach.cshtml");
+            List<tblSach> listSach =  db.tblSaches.Where(
+                row => row.sTensach.Contains(searchInput)).ToList();
+
+            ViewBag.Search = searchInput.Trim();   
+            return View("~/Views/cSach/vSach.cshtml", listSach);
         }
+
+        public ActionResult Detail(string sMasach)
+        {
+            tblSach sach = db.tblSaches.Where(row => row.sMasach ==  sMasach).FirstOrDefault();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(tblSach sach)
+        {
+            db.tblSaches.Add(sach);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Update(tblSach sach)
+        {
+            tblSach sachDB = db.tblSaches.Where(row => row.sMasach == sach.sMasach).FirstOrDefault();
+            sachDB.sTensach = sach.sTensach;
+            sachDB.sTenTG = sach.sTenTG;
+            sachDB.iDongia = sach.iDongia;
+            sachDB.iSoluong = sach.iSoluong;
+            sachDB.sNXB = sach.sNXB;
+            sachDB.sTheloai = sach.sTheloai;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string sMasach)
+        {
+            tblSach sachDB = db.tblSaches.Where(row => row.sMasach == sMasach).FirstOrDefault();
+            db.tblSaches.Remove(sachDB);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
